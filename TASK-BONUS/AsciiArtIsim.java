997eeadc963d0f63adef708b76a765e603d0d8de
ebrarsude <250541104@firat.edu.tr>
2025-10-23 07:33:40 -0700

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("EEEE  BBBB   RRRR    A   RRRR");
+        System.out.println("E     B   B  R   R  A A  R   R");
+        System.out.println("EEE   BBBB   RRRR  AAAAA RRRR");
+        System.out.println("E     B   B  R  R  A   A R  R");
+        System.out.println("EEEE  BBBB   R   R A   A R   R");
+    }
+}
