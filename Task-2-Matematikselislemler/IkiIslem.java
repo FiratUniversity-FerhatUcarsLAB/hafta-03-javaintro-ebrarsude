fc581ae5f25ccb81b6f2fbe41f81823d76c39875
ebrarsude <250541104@firat.edu.tr>
2025-10-23 02:15:27 -0700

    Update IkiIslem.java

--- Task-2-Matematikselislemler/IkiIslem.java
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -1,9 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Java'ya Hos Geldiniz");
-        System.out.println("Java'ya Hos Geldiniz");
-        System.out.println("Java'ya Hos Geldiniz");
-        System.out.println("Java'ya Hos Geldiniz");
-        System.out.println("Java'ya Hos Geldiniz");
-    }
-}
+
