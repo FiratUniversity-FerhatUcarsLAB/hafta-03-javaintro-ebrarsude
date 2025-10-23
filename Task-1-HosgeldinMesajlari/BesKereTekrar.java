0c33b7d1113e6f12b0ff04476ed069b09cc20e03
ebrarsude <250541104@firat.edu.tr>
2025-10-23 02:15:42 -0700

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+    }
+}
