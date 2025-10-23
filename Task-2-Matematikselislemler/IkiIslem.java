586086aef4452d7a180a27c8b4f16dcf27f2063c
ebrarsude <250541104@firat.edu.tr>
2025-10-23 06:54:47 -0700

    Update IkiIslem.java

--- Task-2-Matematikselislemler/IkiIslem.java
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -1,1 +1,10 @@
+public class IkiIslemSonucu {
+    public static void main(String[] args) {
+        double pay = (9.5 * 4.5) - (2.5 * 3);
+        double payda = 45.5 - 3.5;
+        double sonuc = pay / payda;
+
+        System.out.printf("Sonuc = %.4f%n", sonuc);
+    }
+}
 
